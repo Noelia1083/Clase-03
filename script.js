dc2ca4b45110f1df59a88186684f47cff6a3a8e9
Noelia <peraltanoelia5352@gmail.com>
2021-06-07 22:13:15 -0300

    primer commit

---
+++ script.js
@@ -0,0 +1,133 @@
+/*//EJERCICIO 0
+let nombre= prompt("Ingrese su nombre")
+console.log(nombre)
+let saludo= alert("Hola" + " " + nombre)
+let apellido= prompt("Ingrese su apellido")
+console.log( "Hola" + " " + nombre+ " " + apellido )*/
+
+/*/ EJERCICI O1
+let numero1= parseFloat( prompt ("Ingrese un número"))
+let numero2= parseFloat (prompt ("Ingrese otro número"))
+console.log(numero1+numero2)*/
+/*/ EJRCICIO 02
+let numero1= parseInt( prompt ("Ingrese la edad que cumple o cumplió este año "))
+let fecha= 2021-numero1
+console.log ("Usted nacio en "+ fecha)*/
+
+/*//EJERCICIO 03
+let balance= 20000
+let saldo=  alert( "Su saldo es de " + balance)
+let retiro= parseFloat( prompt("Ingrese monto a retirar"))
+let balance1= balance-retiro
+let saldo1= alert ("Su nuevo saldo es " + balance1)*/
+
+/*/EJERCICIO 04
+let nota1=parseInt (prompt("Ingrese una nota"))
+let nota2=  parseInt(prompt ("Ingrese una seguna nota"))
+let nota3= parseInt (prompt("Ingrese una tercera nota"))
+let promedio= (nota1+nota1+nota3)/3
+console.log ("La nota promedio es "+ promedio)*/
+
+/*/EJERCICO 05
+let metros= parseFloat (prompt ("Ingrese una medida en metros"))
+let pies= metros * 3.28084
+console.log ("Los metros ingresados son " + pies + " pies" )
+let centimetros= metros*100
+console.log ("Los metros ingresados son " + centimetros + " centímetros" )
+let pulgadas= metros*39.3701
+console.log ("Los metros ingresados son " + pulgadas + " pulgadas" )*/
+
+/*/EJERCICIO 06
+let numero1=parseFloat (prompt("Ingrese un numero"))
+let numero2=  parseFloat (prompt ("Ingrese un segundo numero"))
+let suma= numero1+numero2
+console.log("La suma de los dos numeros es "+ suma)
+let numero3= parseFloat (prompt ("Ingrese un tercer numero"))
+let multiplicacion= suma*numero3
+console.log ("El resultado final es " + multiplicacion)*/
+
+/*/ EJERCICIO 07
+let minutos= parseFloat (prompt ("Ingrese los minutos"))
+let segundos= minutos*60
+alert  ("Los minutos ingresados equivalen a " + segundos + " segundos" )*/
+
+/*/ EJERCICIO 08
+let base= parseFloat (prompt ("Ingrese la base"))
+let altura= parseFloat (prompt ("Ingrese la altura"))
+let area= base*altura
+alert ("El area del rectángulo es " + area)*/
+
+/*/EJERCICIO 09
+let base= parseFloat (prompt ("Ingrese la base"))
+let altura= parseFloat (prompt ("Ingrese la altura"))
+let area= (base*altura)/2
+alert ("El area del triángulo es " + area)*/
+
+/*/EJERCICIO 10
+let monto= parseFloat (prompt ("Ingrese un monto"))
+let numerodescuento= parseInt (prompt ("Ingrese un número de descuento"))
+let descuento=500
+let total= monto-descuento
+alert ("El monto con descuento es de "+ total)*/
+
+/*/EJERCICIO 11
+let nombre= prompt ("Ingrese su nombre")
+let numero1= parseInt( prompt ("Ingrese la edad que cumple o cumplió este año "))
+let fecha= 2021-numero1
+alert (" Hola " + nombre + " ,usted nacio en el año "+ fecha)*/
+
+/*/EJERCICIO 12
+let celsius= parseFloat (prompt ("Ingrese los grados celsius"))
+let farenheit= (celsius) *(9/5)+32
+alert  ("Los grados ingresados equivalen a " + farenheit + " grados farenheit" )*/
+
+
+/*/EJERCICIO 13
+let farenheit= parseFloat (prompt ("Ingrese los grados farenheit"))
+let celsius= (farenheit - 32) * (5/9)
+alert  ("Los grados ingresados equivalen a " + celsius + " grados celsius" )*/
+
+
+/*/EJERCICIO 14 
+let numero= parseInt (prompt("Ingrese un número"))
+let suma=numero+5
+let multi=suma*10
+let resto=multi%3
+console.log(resto)*/
+
+/*/BONUS 0
+let numero1=  prompt("Ingrese un número")
+let numero2=  prompt("Ingrese un número")
+let suma= numero1+numero2
+console.log (suma)*/
+
+/*/BONUS 1
+let dias= parseFloat (prompt ("Ingrese una cantidad de dias"))
+let segundos= dias*86400
+alert  ("Los dias ingresados equivalen a " + segundos + " segundos" )*/
+
+/*/ BONUS 2
+let kilómetros= parseFloat (prompt ("Ingrese la cantidad de kilómetros"))
+let velocidad= parseFloat (prompt ("Ingrese la velocidad promedio a la que desea ir"))
+let tiempohora= kilómetros/velocidad
+let tiempominutos=tiempohora*60
+console.log(tiempominutos)*/
+
+//BONUS 3
+let bruto= parseFloat (prompt ("Ingrese el monto del sueldo bruto"))
+let obrasocial=(bruto*3)/100
+let jubilación=(bruto*11)/100
+let ley19032=(bruto*3)/100
+let neto= bruto-obrasocial-jubilación-ley19032
+alert ( "Su sueldo bruto es de  " + neto)
+
+
+
+
+
+
+
+
+
+
+
